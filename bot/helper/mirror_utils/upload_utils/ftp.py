efe381d2ceb03b4ee503139096b340d32e8bae20
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-03-05 12:13:11 +0530

    Update ftp.py

--- bot/helper/mirror_utils/upload_utils/ftp.py
+++ bot/helper/mirror_utils/upload_utils/ftp.py
@@ -2,14 +2,30 @@ from ftplib import FTP
 import ftplib
 import os,subprocess
 
+LOGGER.info(f"FTP name: {up_name}")
 ftp = FTP(FTP_SERVER)
 ftp.login(FTP_USER,FTP_PASSWORD)
-m_path= 
-path=m_path+".zip" 
-subprocess.run(["7z","a","-mx=0", path, m_path])
-import os.path
-ftp.set_pasv(True)
-totalSize = os.path.getsize(path)
-print('Total file size : ' + str(round(totalSize / 1024 / 1024 ,1)) + ' Mb')
-file = open(path, 'rb')
-ftp.storbinary(f"STOR {path}", file)
+def files(path):
+ for file in os.listdir(path):
+  if os.path.isfile(os.path.join(path, file)):
+    yield file
+path ="downloads"
+up_file = up_name
+up_path = glob.glob(os.path.join(path, '*'))
+total_files = len(up_path)
+loop = 0
+while not total_files == loop:
+  file_name=up_path[loop]
+  path2 = glob.glob(os.path.join(file_name, '*'))
+  path2 = str(path2[0])
+  loop = loop+1
+  for file in files(file_name):
+   if file == up_file:
+   ftp.set_pasv(True)
+   file = open(path2, 'rb')
+   up = "Uploading to FTP Server 📤"
+   x = bot.send_message(up, self.bot, self.update)
+   ftp.storbinary(f"STOR {up_name}", file)
+   up = f"{up_name} is Uploaded ✅"
+   bot.edit_message_text(x, up, self.bot, self.update)
+
