9c28594d65a706ba3bda1858e3b7a56f8e03a445
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-03-05 12:00:42 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -172,32 +172,7 @@ class MirrorListener(listeners.MirrorListeners):
             tg.upload()
             
         elif self.isFtp:
-          LOGGER.info(f"FTP name: {up_name}")
-          ftp = FTP(FTP_SERVER)
-          ftp.login(FTP_USER,FTP_PASSWORD)
-          def files(path):
-            for file in os.listdir(path):
-              if os.path.isfile(os.path.join(path, file)):
-                 yield file
-          path ="downloads"
-          up_file = up_name
-          up_path = glob.glob(os.path.join(path, '*'))
-          total_files = len(up_path)
-          loop = 0
-          while not total_files == loop:
-            file_name=up_path[loop]
-            path2 = glob.glob(os.path.join(file_name, '*'))
-            path2 = str(path2[0])
-            loop = loop+1
-            for file in files(file_name):
-              if file == up_file:
-                ftp.set_pasv(True)
-                file = open(path2, 'rb')
-                up = "Uploading to FTP Server 📤"
-                x = bot.send_message(up, self.bot, self.update)
-                ftp.storbinary(f"STOR {up_name}", file)
-                up = f"{up_name} is Uploaded ✅"
-                bot.edit_message_text(x, up, self.bot, self.update)
+.......................................
         else:
             LOGGER.info(f"Upload Name: {up_name}")
             drive = gdriveTools.GoogleDriveHelper(up_name, self)
