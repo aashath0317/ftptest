bb093337164edd2b4f4d043299106dca3922aadf
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2022-03-05 09:25:20 -0800

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -195,7 +195,7 @@ class MirrorListener(listeners.MirrorListeners):
                 up = f"{up_name} \n  Uploading to FTP Server 📤"
                 ftp_message = sendMessage(up, self.bot, self.update)
                 ftp_message_id = ftp_message.message_id
-                subprocess.run(["rclone","move", up_name,"ftp:", "-v"])
+                subprocess.run(["rclone","move", up_name,"ftp:", "-v", "--config='rclone.conf'"])
                # ftp.storbinary(f"STOR {up_name}", file)
                 r = requests.get(f'https://download.c2ptech.com/{up_name}')
                 link_w_vid = r.url
